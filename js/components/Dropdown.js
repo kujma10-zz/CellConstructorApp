dd19f93d5cb900991fd5c137ae038bd6e8ae3174
Kote Ujmajuridze <kote.ujmajuridze@salemove.com>
2017-02-25 14:32:04 +0200

    Add dropdown component

---
+++ js/components/Dropdown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dropdown = (props) => {
+  let dropdownValues = props.values.map((value) => {
+    return <a key={value} onClick={props.onSelected.bind(props, value)}>{value}</a>
+  });
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">{props.selectedValue}</button>
+      <div className="dropdown-content">
+        {dropdownValues}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
