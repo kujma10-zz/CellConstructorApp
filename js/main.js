3cbc40c6f8840af8122b5c001c4590be03653638
Kote Ujmajuridze <kote.ujmajuridze@salemove.com>
2017-03-04 19:34:48 +0200

    Add redux dev tools

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reactionsReducer from './reducers'
 
-let store = createStore(reactionsReducer)
+let store = createStore(
+  reactionsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
